38642075bfa860739d5ef160180a40cd7a36ead9
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2024-04-13 12:08:04 -0400

    je vais aller manger

--- Godot Wild-jam 68/Scripts/Spaceship/ForgeComponent.gd
+++ Godot Wild-jam 68/Scripts/Spaceship/ForgeComponent.gd
@@ -10,6 +10,8 @@ var forging: Item
 
 @onready var panel = $"../Panel"
 @onready var timer = $Timer
+@onready var h_box_container = $"../Panel/HBoxContainer"
+const ITEM = preload("res://Scenes/Forge/item.tscn")
 
 func _ready():
 	timer.wait_time = forge_cooldown[forge_tier - 1]
@@ -26,7 +28,23 @@ func start_choosing():
 	
 	timer.stop()
 	
-	# TODO
+	unload_last_choices()
+	load_choices()
+
+func unload_last_choices():
+	for i in h_box_container.get_children():
+		i.queue_free()
+
+func load_choices():
+	var items = Inventory.load_products_from_tier(forge_tier)
+	
+	for i in items:
+		var item = ITEM.instantiate()
+		# item.icon = i.icon
+		h_box_container.add_child(item)
+	
+	panel.size = Vector2(h_box_container.size.x + 8.0, h_box_container.size.y)
+	panel.position = Vector2(panel.size.x / -2,-160.0)
 
 func _on_timer_timeout():
 	var parent = get_parent()
