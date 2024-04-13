a825437f162f4dc679843ceca71465bebecf0987
Louis Emery <emerylo@educ.college-montreal.qc.ca>
2024-04-12 23:02:12 -0400

    shop ui

--- Godot Wild-jam 68/Scripts/Items/Shop_item_button.gd
+++ Godot Wild-jam 68/Scripts/Items/Shop_item_button.gd
@@ -8,8 +8,16 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var button = Button.new()
 	text = item.name
-	#price_button.text = item.price
+	price_button.text = str(item.price)
+	button.pressed.connect(self._button_pressed)
+	
+func _button_pressed():
+	item_image = item.name
+	price = item.price
+	item_image = item.icon
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
