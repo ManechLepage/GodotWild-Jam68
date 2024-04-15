bd85843f5954b38b750a3a4a021123043c07b5a5
AquaFly1 <emerylo@educ.college-montreal.qc.ca>
2024-04-14 21:16:03 -0400

    WIKI KI MARCHE

--- Godot Wild-jam 68/wiki_button.gd
+++ Godot Wild-jam 68/wiki_button.gd
@@ -1,10 +1,11 @@
 extends Button
-@onready var item_name = $Panel2/Panel/Name
-@onready var description = $Panel2/Panel2/Description
-@onready var use = $Panel2/Panel3/use
-@onready var item_icon = $Panel2/Icon
+
 @onready var button = $"."
 @export var item: Item
+@onready var item_name = $"../../../Panel2/Panel/Name"
+@onready var description = $"../../../Panel2/Panel2/Description"
+@onready var use = $"../../../Panel2/Panel3/use"
+@onready var item_icon = $"../../../Panel2/Icon"
 
 
 # Called when the node enters the scene tree for the first time.
