01010274047fb0f8ded6abcecb308e2ac6ca437b
AquaFly1 <emerylo@educ.college-montreal.qc.ca>
2024-04-14 20:22:40 -0400

    wiki

--- Godot Wild-jam 68/Scripts/Items/Shop_item_button.gd
+++ Godot Wild-jam 68/Scripts/Items/Shop_item_button.gd
@@ -1,6 +1,5 @@
 extends Button
 
-<<<<<<< Updated upstream
 var tier: int
 var section: AnimatedSprite2D
 
@@ -17,22 +16,3 @@ func load_values(section, tier, price):
 
 func _on_pressed():
 	pass
-=======
-@onready var description = $"../../../Panel2/Panel2/Description"
-@onready var item_icon = $"../../../Panel2/Icon"
-@export var item: Item
-@onready var item_name = $"../../../Panel2/Panel/Name"
-@onready var use = $"../../../Panel2/Panel3/use"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	text = item.name
-	
-	
-func _on_pressed():
-	item_name.text = item.name
-	use.text = item.use
-	description.text = item.description
-	item_icon.texture = item.icon
->>>>>>> Stashed changes
