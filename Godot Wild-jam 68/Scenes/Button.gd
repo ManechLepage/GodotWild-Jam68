7b272d907d11d598ad1e37e4a1c6042064cb5df9
AquaFly1 <emerylo@educ.college-montreal.qc.ca>
2024-04-14 20:17:14 -0400

    wiki

---
+++ Godot Wild-jam 68/Scenes/Button.gd
@@ -0,0 +1,15 @@
+extends Button
+const Wiki_scene = ("res://Scenes/UI/Wiki.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	get_tree().change_scene_to_file(Wiki_scene)
