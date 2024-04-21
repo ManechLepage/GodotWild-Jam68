e219cd3a85f2efc2cb83e8479e304f6e0a7157bc
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2024-04-21 11:14:23 -0400

    Revert "recipes"
    This reverts commit 8eb5f0a92a5f3c1330535e16d5dafe042853ce25.

--- Godot Wild-jam 68/Scenes/Menu.gd
+++ Godot Wild-jam 68/Scenes/Menu.gd
@@ -4,14 +4,11 @@ extends Control
 @onready var inventory_ctrl = %InventoryCtrl
 @onready var customer_ctrl = %CustomerCtrl
 @onready var money_ctrl_2 = %MoneyCtrl2
-@onready var back_button = $"../Back_button"
-
 
 func _on_button_pressed():
 	main_2d.visible = true
 	inventory_ctrl.visible = true
 	customer_ctrl.visible = true
 	money_ctrl_2.visible = true
-	back_button.visible = false
 	
 	visible = false
---
+++ Godot Wild-jam 68/Scripts/UI/button_forge.gd
@@ -0,0 +1,6 @@
+extends Button
+
+var item:Item
+
+func _on_pressed():
+	get_parent().button_pressed(self)
